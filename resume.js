edc80f2ac1a341ee8eae56579cff122b539d6fb8
pratham-192 <mehtani2020@gmail.com>
2022-09-28 02:01:27 +0530

    skills bar animation added

--- resume.js
+++ resume.js
@@ -22,3 +22,50 @@ for(var i=0;i<navmenuanchortags.length;i++)
         console.log(targetsection);
     })
 }
+/// adding the skill section js feature
+// 1) handle scroll event
+// 2) skill section is visible or not on screen
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillscontainer=document.getElementById("skill-container");
+window.addEventListener('scroll',checkScroll);
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+var animationdone=false;
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var coords = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (coords.top <= (window.innerHeight - coords.height))) {
+            bar.setAttribute("data-visited", true);
+            fillbars(bar);
+        } else if (coords.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+function fillbars(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
